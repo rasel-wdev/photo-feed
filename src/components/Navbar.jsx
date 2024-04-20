d22d581be42b03a09499867c904dbf8979acd92f
Md. Rasel Rana <rasel.learn22@gmail.com>
2024-04-20 19:17:02 +0600

    The language switcher option added in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import LanguageSwitcher from "@/components/LanguageSwitcher";
 import Logo from "./Logo";
 
 export default function Navbar() {
@@ -6,26 +7,8 @@ export default function Navbar() {
       <div class="container mx-auto flex items-center justify-between gap-x-6">
         <div class="flex gap-4 items-center">
           <Logo />
-          {/* <!-- Language Dropdown -->
-        <div class="relative">
-          <button class="flex items-center gap-2">
-            <img class="max-w-8" src="./assets/images/bd.png" alt="bangla" />
-            Bangla
-          </button>
-
-          <!-- dropdown -->
-          <div class="absolute right-0 top-full mt-2 w-40 rounded-md bg-white p-2 z-10 shadow-lg">
-            <li class="flex items-center gap-2 p-2 rounded-md cursor-pointer hover:bg-gray-100">
-              <img class="max-w-8" src="./assets/images/bd.png" alt="bangla" />
-              Bangla
-            </li>
-            <li class="flex items-center gap-2 p-2 rounded-md cursor-pointer hover:bg-gray-100">
-              <img class="max-w-8" src="./assets/images/usa.png" alt="bangla" />
-              English
-            </li>
-          </div>
-        </div> */}
         </div>
+        <LanguageSwitcher />
       </div>
     </nav>
   );
